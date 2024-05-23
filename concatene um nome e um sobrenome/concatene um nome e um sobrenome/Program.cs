a1c67dca072c80668db74815311889cd8f244a70
Anderson <andersondapper@gmail.com>
2024-05-23 19:02:40 -0300

    first commit

---
+++ concatene um nome e um sobrenome/concatene um nome e um sobrenome/Program.cs
@@ -0,0 +1,34 @@
+﻿//Crie um programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exiba o nome completo.
+using System;
+using System.Globalization;
+
+namespace Program 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            CultureInfo CI = CultureInfo.InstalledUICulture;
+            
+            string nome1, nome2;
+            int idade;
+
+                   
+                      
+            Console.WriteLine("Digite seu primeiro nome:");
+            nome1 = Console.ReadLine();
+
+            Console.WriteLine("Digite seu sobre-nome:");
+            nome2 = Console.ReadLine();
+
+            Console.WriteLine("Insira sua idade:");
+            idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Seu nome completo é:" + " " + nome1 + " " + nome2 + " " + "e tem" + " " + idade + " " + "anos de idade.");
+           
+
+        }
+        
+    }
+
+} 
